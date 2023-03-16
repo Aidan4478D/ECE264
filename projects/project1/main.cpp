88aaa5848027cbe34a86555742c56a84eed5ced9
Aidan Cusa <aidancusa@gmail.com>
2023-03-16 19:03:39 -0400

    kinda works, parsing, and packing

--- projects/project1/main.cpp
+++ projects/project1/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <vector>
 
+#include <fstream>
+#include <sstream>
+#include <vector> 
+
+
+
 using namespace std; 
 
 
@@ -10,7 +16,7 @@ class foobar {
   int     cur_pos; 
 
   protected:
-    int pos = cur_pos;  
+    int pos;  
 
   public:
     
@@ -22,7 +28,7 @@ class foobar {
     string   get_name(void); 
 
     //virtual function   
-    virtual int  get_strength() {return 0;};
+    virtual int  get_strength();
 };
 
 
@@ -46,6 +52,19 @@ foobar::foobar(string fb_name = "", int fb_pos = 0) {
 
   name = fb_name; 
   cur_pos = fb_pos; 
+}
+
+foobar &foobar::set_position(int position) {
+
+  cur_pos = position;
+  pos = cur_pos; 
+
+  return *this; 
+}
+
+int foobar::get_strength() {
+
+  return pos;  
 }
 
 
@@ -82,14 +101,98 @@ int bar::get_strength() {
 
 
 
-int main() {
+foobar *pack_vector(string fb_type, string name) {
 
+  if(fb_type.compare("foobar") == 0) {
+
+    foobar *fb = new foobar(name, 0);
+    return fb;  
+  }
+  if(fb_type.compare("foo") == 0) {
+
+    foo *f = new foo(name, 0); 
+    return f;
+  }
+  if(fb_type.compare("bar") == 0) {
+    
+    bar *b = new bar(name, 0);
+    return b;  
+  }
+
+  return NULL; 
+}
+
+
+
+
+void parse_file(string file_name, vector<foobar *> fb_vector) {
+
+  ifstream f (file_name); 
+  string line = ""; 
+  
+  if(f.is_open()) {
+
+    while(f) {
+
+      getline(f, line);
+      
+      stringstream s(line);
+      string word, fb_type, name; 
+   
+      int count = 0;    
+      while(s >> word) {
+
+        count == 0 ? fb_type = word : name = word; 
+      }
+
+      fb_vector.push_back(pack_vector(fb_type, word)); 
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+  
+  /*
   string n1 = "Mushu"; 
   int pos = 1; 
 
   foobar *fb1 = new foobar(n1, pos);   
 
   cout << "name is " << fb1->get_name() << "\n";  
+  cout << "strenght is " << fb1->get_strength() << "\n"; 
+
+  fb1->set_position(5); 
+
+  cout << "strenght is " << fb1->get_strength() << "\n"; 
+
+
+  foo *f1 = new foo("george", pos); 
+  bar *b1 = new bar("ming", pos); 
+
+  f1->set_position(3); 
+  b1->set_position(5); 
+
+  cout << "name is " << f1->get_name() << "\n";  
+  cout << "strenght is " << f1->get_strength() << "\n"; 
+
+  cout << "name is " << b1->get_name() << "\n";  
+  cout << "strenght is " << b1->get_strength() << "\n"; 
+  */
+  vector<foobar *> fb_vector; 
+
+  parse_file("sample_text.txt", fb_vector); 
+  
 
 
   return 0; 
